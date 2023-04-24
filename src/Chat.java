889104ef7860e526bea32e25400b59c65e02aa94
Luis <luisferang2013@gmail.com>
2023-04-24 00:21:28 -0700

    Added some SQLite, Worked on initScreen + ChatData methods

--- src/Chat.java
+++ src/Chat.java
@@ -6,46 +6,10 @@ import java.sql.Statement;
 import java.io.IOException;
 
 public class Chat {
-	private final String url = "jdbc:postgresql://localhost/chat";
-	private final String usr = "postgres";
-	private final String pswd = "chat";
 	
-	public static void main(String[] args) {
-		
-		Chat chat = new Chat();
-		
-		Connection con = chat.connect();
-		
-		Statement stmt = chat.getStatement(con);
-		
-		boolean quit = false;
-	}
 	
-	private Connection connect() {
-		Connection con = null;
-		
-		try {
-			con = DriverManager.getConnection(url, usr, pswd);
-			
-			System.out.println("Connected to database successfully");
-		}catch(SQLException e) {
-			//System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-		
-		return con;
-	}
 	
-	private Statement getStatement(Connection con) {
-		Statement stmt = null;
-		
-		try {
-			stmt = con.createStatement();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		System.out.println("Statement created successfully");
-		return stmt;
-	}
+	
+	
 
 }
